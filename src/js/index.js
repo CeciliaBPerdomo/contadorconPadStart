4612fb96fc1f1d495db3bd1d6136c0ef8a780c67
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2023-01-25 23:05:33 +0000

    Contador con padStart()

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,31 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+
+function tick() {
+    counter++git r
+    let arraycounter = counter.toString().padStart(6,0).split('');
+    
+    const element = (
+        
+        <div className=" container d-flex justify-content-center" 
+        style={{marginTop: "25px", color: "white"}}>
+            <h1 className="text-center" style={{color: "red"}}>C O N T A D O R</h1>
+            <br />
+
+            <div className="d-flex" style={{marginTop: "100px"}}>
+                <i className="fa fa-clock" style={{fontSize: "40px"}}></i>
+            {
+                arraycounter.map((count,i)=>(
+                    <h2 key = {i} className="text-center" style={{marginLeft: "95px"}}>{count}</h2>            
+                ))
+            }
+            </div>
+      </div>
+    );
+    ReactDOM.render(element,document.querySelector("#app"));
+  }
+  
+  setInterval(tick, 1000);
+//ReactDOM.render( < Home / > , document.querySelector("#app"));
